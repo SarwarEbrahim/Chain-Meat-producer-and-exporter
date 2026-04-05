6d7bbab5633878acfc797cdd9629d29934616549
SarwarEbrahim <ebrahimsarwar2015@gmail.com>
2026-04-06 02:48:25 +0600

    updated

--- src/main/java/com/example/chainmeatproducerandexporter/EbrahimSarwar/supplierHomePageController.java
+++ src/main/java/com/example/chainmeatproducerandexporter/EbrahimSarwar/supplierHomePageController.java
@@ -1,41 +1,54 @@
 package com.example.chainmeatproducerandexporter.EbrahimSarwar;
 
 import javafx.event.ActionEvent;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class supplierHomePageController
 {
     @javafx.fxml.FXML
-    private Button ViewMeatTypesOnAction;
-    @javafx.fxml.FXML
-    private Button viewApprovalStatusOnAction;
-    @javafx.fxml.FXML
-    private Button viewPaymentStatusOnAction;
-    @javafx.fxml.FXML
     private VBox vBox;
     @javafx.fxml.FXML
-    private Button menuOnAction;
-    @javafx.fxml.FXML
     private Label personalInfoOutput;
-    @javafx.fxml.FXML
-    private Button viewUnitPriceOnAction;
-    @javafx.fxml.FXML
-    private Button choosePaymentMethodOnAction;
-    @javafx.fxml.FXML
-    private Button scheduledDeliveryOnAction;
 
     @javafx.fxml.FXML
     public void initialize() {
+        vBox.setVisible(false);
 
     }
 
 
+    @javafx.fxml.FXML
+    public void viewPaymentStatusOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void viewApprovalStatusOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void ViewMeatTypesOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void menuOnAction(ActionEvent actionEvent) {
+        vBox.setVisible(true);
+    }
+
+    @javafx.fxml.FXML
+    public void viewUnitPriceOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void choosePaymentMethodOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void scheduledDeliveryOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void SelectedMeatTypesOnAction(ActionEvent actionEvent) {
+    }
 }

309b6097f24ba7ad16ee93977f87c5575150bb09
SarwarEbrahim <ebrahimsarwar2015@gmail.com>
2026-04-06 00:33:47 +0600

    hello

--- src/main/java/com/example/chainmeatproducerandexporter/EbrahimSarwar/supplierHomePageController.java
+++ src/main/java/com/example/chainmeatproducerandexporter/EbrahimSarwar/supplierHomePageController.java
@@ -1,7 +1,41 @@
 package com.example.chainmeatproducerandexporter.EbrahimSarwar;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class supplierHomePageController
 {
     @javafx.fxml.FXML
+    private Button ViewMeatTypesOnAction;
+    @javafx.fxml.FXML
+    private Button viewApprovalStatusOnAction;
+    @javafx.fxml.FXML
+    private Button viewPaymentStatusOnAction;
+    @javafx.fxml.FXML
+    private VBox vBox;
+    @javafx.fxml.FXML
+    private Button menuOnAction;
+    @javafx.fxml.FXML
+    private Label personalInfoOutput;
+    @javafx.fxml.FXML
+    private Button viewUnitPriceOnAction;
+    @javafx.fxml.FXML
+    private Button choosePaymentMethodOnAction;
+    @javafx.fxml.FXML
+    private Button scheduledDeliveryOnAction;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+
+    }
+
+
+}

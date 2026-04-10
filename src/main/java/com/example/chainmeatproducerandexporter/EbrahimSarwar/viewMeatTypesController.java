b5ef485d3fcfebc5a3b7147891021c4766e0040b
SarwarEbrahim <ebrahimsarwar2015@gmail.com>
2026-04-10 22:11:13 +0600

    updated

--- src/main/java/com/example/chainmeatproducerandexporter/EbrahimSarwar/viewMeatTypesController.java
+++ src/main/java/com/example/chainmeatproducerandexporter/EbrahimSarwar/viewMeatTypesController.java
@@ -1,15 +1,75 @@
 package com.example.chainmeatproducerandexporter.EbrahimSarwar;
 
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
 
 public class viewMeatTypesController
+
+
+
 {
+
+
     @javafx.fxml.FXML
-    private TableColumn meatTypeColumn;
-    @javafx.fxml.FXML
-    private TableView meatTypesTable;
+    private Label quantityOutput;
 
     @javafx.fxml.FXML
     public void initialize() {
-    }}
+
+    }
+
+    @javafx.fxml.FXML
+    public void selectButton1(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void selectButton3(ActionEvent actionEvent) {
+
+    }
+
+    @javafx.fxml.FXML
+    public void quantityButton6(ActionEvent actionEvent) {
+        quantityOutput.setText("50kg");
+    }
+
+    @javafx.fxml.FXML
+    public void selectButton2(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void quantityButton5(ActionEvent actionEvent) {
+        quantityOutput.setText("110kg");
+    }
+
+    @javafx.fxml.FXML
+    public void selectButton5(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void selectButton4(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void quantityButton2(ActionEvent actionEvent) {
+        quantityOutput.setText("200kg");
+    }
+
+    @javafx.fxml.FXML
+    public void selectButton6(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void quantityButton1(ActionEvent actionEvent) {
+        quantityOutput.setText("250kg");
+    }
+
+    @javafx.fxml.FXML
+    public void quantityButton4(ActionEvent actionEvent) {
+        quantityOutput.setText("160kg");
+    }
+
+    @javafx.fxml.FXML
+    public void quantityButton3(ActionEvent actionEvent) {
+        quantityOutput.setText("140kg");
+    }
+}

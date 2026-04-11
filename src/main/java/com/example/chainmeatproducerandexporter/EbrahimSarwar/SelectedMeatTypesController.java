11799e6e346be2adb0f061416b1f9ee085cb1ea4
SarwarEbrahim <ebrahimsarwar2015@gmail.com>
2026-04-11 20:11:04 +0600

    updated

--- src/main/java/com/example/chainmeatproducerandexporter/EbrahimSarwar/SelectedMeatTypesController.java
+++ src/main/java/com/example/chainmeatproducerandexporter/EbrahimSarwar/SelectedMeatTypesController.java
@@ -1,21 +1,73 @@
 package com.example.chainmeatproducerandexporter.EbrahimSarwar;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class SelectedMeatTypesController
 {
     @javafx.fxml.FXML
+    private TableView<MeatType> selectedMeatTable;
+    @javafx.fxml.FXML
+    private TableColumn<MeatType, String> selectedMeatColumn;
+    @javafx.fxml.FXML
+    private Label selectedMeatLabel;
+
+    @javafx.fxml.FXML
     public void initialize() {
+        selectedMeatColumn.setCellValueFactory(new PropertyValueFactory<MeatType, String>("meat"));
+
     }
 
     @javafx.fxml.FXML
     public void loadMeatTypesOnAction(ActionEvent actionEvent) throws FileNotFoundException {
+        ArrayList<MeatType> meatTypeList = new ArrayList<MeatType>();
         File f = new File("requiredMeat.txt");
         Scanner s = new Scanner(f);
+        String str = "";
+
+        while (s.hasNextLine()) {
+            str = s.nextLine();
+            meatTypeList.add(new MeatType(str));
+        }
+        selectedMeatTable.getItems().clear();
+        selectedMeatTable.getItems().addAll(meatTypeList);
         
+    }
+
+    @javafx.fxml.FXML
+    public void confirmOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void increaseQuantity(ActionEvent actionEvent) {
+        selectedMeatTable.getSelectionModel().getSelectedItem();
+
+    }
+
+    @javafx.fxml.FXML
+    public void decreaseQuantity(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void backHomeOnAction(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/chainmeatproducerandexporter/supplierHomePage.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
     }
 }
--- src/main/java/com/example/chainmeatproducerandexporter/EbrahimSarwar/viewMeatTypesController.java
+++ src/main/java/com/example/chainmeatproducerandexporter/EbrahimSarwar/viewMeatTypesController.java
@@ -1,7 +1,12 @@
 package com.example.chainmeatproducerandexporter.EbrahimSarwar;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -115,4 +120,15 @@ public class viewMeatTypesController
     public void quantityButton3(ActionEvent actionEvent) {
         quantityOutput.setText("140kg");
     }
+
+    @javafx.fxml.FXML
+    public void backHomeOnAction(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/chainmeatproducerandexporter/supplierHomePage.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+
 }
